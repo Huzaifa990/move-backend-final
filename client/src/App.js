3913f1b6ad0708df92a618b4f3f1b7d982877603
abbasmoiz7 <abs.mv07@gmail.com>
2022-11-24 17:55:46 +0500

    Message

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,29 @@ import Search from "./components/Search";
 import Introduction from "./components/Introduction";
 import SubServices from "./components/Services/SubServices";
 import Discount from "./components/Services/Discount";
-import Slider from "./components/Headers/slider";
+import Slider from "./components/Headers/Slider";
+import SubHeaderAbout from "./components/Headers/SubHeader";
+import FindCar from "./components/FindCar";
+import Footer from "./components/Footer";
+import ContactUs from "./components/ContactUs";
+
 function App() {
   return (
     <div className="App">
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Rubik&display=swap" rel="stylesheet"/>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet"/>
+
       <NavBar />
       <Search />
       <Slider />
+      {/* <SubHeaderAbout/> */}
       <Introduction />
       <SubServices />
       <Discount />
+      <FindCar/>
+      <ContactUs/>
+      <Footer/>
     </div>
   );
 }
--- client/src/components/Headers/slider.js
+++ client/src/components/Headers/slider.js
@@ -1,105 +1,50 @@
-import React, { useState,
-  //  useEffect 
-  } from "react";
- import M1 from '../../img/M1.jpg';
- import M2 from '../../img/M2.jpg';
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import React from "react";
+import M1 from '../../img/M1.jpg';
+import M2 from '../../img/M2.jpg';
 
-function Slider() {
-  const [index, setIndex] = useState(0);
+const Slider = () => {
+          return (
+            <div className="container-fluid p-0" style={{marginBottom: '90px'}}>
+              <div id="header-carousel" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img className="w-100" src={M1} alt="Image" />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                      <div className="p-3" style={{maxWidth: '900px'}}>
+                        <h4 className="text-white text-uppercase mb-md-3">Rent A Car</h4>
+                        <h1 className="display-1 text-white mb-md-4">
+                          Best Rental Cars In Your Location
+                        </h1>
+                        <a href className="btn btn-primary py-md-3 px-md-5 mt-2">Reserve Now</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carousel-item">
+                    <img className="w-100" src={M2} alt="Image" />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                      <div className="p-3" style={{maxWidth: '900px'}}>
+                        <h4 className="text-white text-uppercase mb-md-3">Rent A Car</h4>
+                        <h1 className="display-1 text-white mb-md-4">
+                          Quality Cars with Unlimited Miles
+                        </h1>
+                        <a href className="btn btn-primary py-md-3 px-md-5 mt-2">Reserve Now</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <a className="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                  <div className="btn btn-dark" style={{width: '45px', height: '45px'}}>
+                    <span className="carousel-control-prev-icon mb-n2" />
+                  </div>
+                </a>
+                <a className="carousel-control-next" href="#header-carousel" data-slide="next">
+                  <div className="btn btn-dark" style={{width: '45px', height: '45px'}}>
+                    <span className="carousel-control-next-icon mb-n2" />
+                  </div>
+                </a>
+              </div>
+            </div>
+          );
+};
 
-  const handleNext = () => {
-    setIndex(index + 1);
-    if (index === 1) {
-      setIndex(0);
-    }
-  };
-  const handlePrev = () => {
-    setIndex(index - 1);
-    if (index === 0) {
-      setIndex(1);
-    }
-  };
-
-  return (
-    <>
-      <div
-        className="relative -top-50"
-      >
-       
-        <div className="overflow-hidden">
-          <div
-            className={`${
-              index === 0 ? "flex" : "hidden"
-            } `}
-          >
-            <img
-              className="h-screen w-full object-cover z-0"
-              alt=""
-              src={M1}
-            />
-          </div>
-          
-          <div
-            className={`${
-              index === 1 ? "flex" : "hidden"
-            }`}
-          >
-            <img
-              className="h-screen w-full object-cover z-0"
-              alt=""
-              src={M2}
-            />
-          </div>
-        </div>
-
-                     <div className={`${index === 0 ? "flex" : "hidden"}`}>
-                 <div className="w-full absolute top-[40%] flex justify-center flex flex-col ">
-                     <h1 className="flex font-medium justify-center font-bold text-5xl text-white">RENT A CAR</h1>
-                     <h1 className="flex mt-4 font-medium justify-center font-bold text-8xl text-white">Best Rental Cars
-                         In</h1>
-                     <h1 className="flex mt-4  justify-center font-bold text-8xl text-white">Your
-                         Location</h1>
-                 </div>
-             </div>
-
-             <div className={`${index === 1 ? "flex" : "hidden"}`}>
-                 <div className="w-full absolute top-[40%]  justify-center flex flex-col ">
-                     <h1 className="flex  justify-center font-bold text-5xl text-white">RENT A CAR</h1>
-                     <h1 className="flex mt-4  justify-center font-bold text-8xl text-white">Quality Cars
-                         With</h1>
-                     <h1 className="flex mt-4 justify-center font-bold text-8xl text-white">Unlimited
-                         Miles</h1>
-                 </div>
-             </div>
-             <div className="w-full h-16 absolute top-[70%] flex justify-center">
-                 <button className="flex px-14 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     Reserve Now
-                 </button>
-             </div>
-
-        {/* <div className="w-full absolute sm:top-[20%] md:top-[27.5%] lg:top-[40.5%] xl:top-[44.5%] flex items-start justify-center">
-          <button className="flex items-start justify-start px-14 py-2 2xl:px-28 2xl:py-5 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-6xl font-medium bg-yellow-400 ">
-            BUY NOW
-          </button>
-        </div> */}
-        <button
-          type="button"
-          className="sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 xl:h-13 xl:w-13 2xl:w-20 2xl:h-20  bg-[#11111b6d] flex absolute sm:top-20 md:top-36 lg:top-48 xl:top-64 2xl:top-96 2xl:left-16 left-10 z-30 justify-center items-center sm:px-1 md:px-2 lg:px-2.5 xl:px-4 cursor-pointer"
-          onClick={handlePrev}
-        >
-          <IoIosArrowBack className="h-24 w-24 font-normal" />
-        </button>
-        <button
-          type="button"
-          className="sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 xl:h-13 xl:w-13 2xl:w-20 2xl:h-20 bg-[#11111b6d] flex absolute sm:top-20 md:top-36 lg:top-48 xl:top-64 2xl:top-96 2xl:right-16 right-10 z-30 justify-center items-center sm:px-1 md:px-2 lg:px-2.5 xl:px-4 cursor-pointer"
-          onClick={handleNext}
-        >
-          <IoIosArrowForward className="h-24 w-24 font-normal" />
-        </button>
-      </div>
-    </>
-  );
-}
-
-export default Slider;
+export default Slider;
