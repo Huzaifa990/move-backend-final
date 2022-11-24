8b1213305a214b85744c827026d0b1547ef12087
abbasmoiz7 <abs.mv07@gmail.com>
2022-11-24 17:39:04 +0500

    Find Car

--- client/src/components/FindCar.js
+++ client/src/components/FindCar.js
@@ -1,206 +1,169 @@
 import React from "react";
-import { flushSync } from "react-dom";
-import Nigga from '../../assests/Images/NiggaCar.jpg';
-import {AiFillCar} from 'react-icons/ai';
-import {MdSettingsSuggest} from 'react-icons/md';
-import {ImRoad} from 'react-icons/im';
-import {HiLocationMarker} from 'react-icons/hi';
+import '../index.css'
+import Car1 from '../img/c180.jpg';
+import Car2 from '../img/m3.jpg';
+
+
 function FindCar(){
     return (
-        <div style={{display: 'grid',  justifyContent:'center', alignItems:'center'}}>
-            <div style={{display: "flex", justifyContent:'center', alignItems:'center'}}>
-                <h1 style={{color: '#f77d0a', fontSize: '150px', justifyItems:'center',fontWeight:'bold'}}>03</h1>
+            <div className="container-fluid py-5">
+              <div className="container pt-5 pb-3">
+                <h1 className="display-1 text-primary text-center">03</h1>
+                <h1 className="display-4 text-uppercase text-center mb-5">Find Your Car</h1>
+                <div className="row">
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car1} alt="" />
+                      <h4 className="text-uppercase mb-4">Mercedes Benz C180</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2015</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>AUTO</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>25K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>LAHORE</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$99.00/Day</a>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item active mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car2} alt="" />
+                      <h4 className="text-uppercase mb-4">BMW M3 COMPETITION</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2019</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>MANUAL</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>55K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>Karachi</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$100.00/Day</a>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car1} alt="" />
+                      <h4 className="text-uppercase mb-4">Honda Civic Type-R</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2019</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>MANUAL</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>55K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>Karachi</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$100.00/Day</a>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car1} alt="" />
+                      <h4 className="text-uppercase mb-4">Mercedes Benz C180</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2015</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>AUTO</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>25K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>LAHORE</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$99.00/Day</a>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item active mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car2} alt="" />
+                      <h4 className="text-uppercase mb-4">BMW M3 COMPETITION</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2019</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>MANUAL</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>55K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>Karachi</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$100.00/Day</a>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 mb-2">
+                    <div className="rent-item mb-4">
+                      <img className="img-fluid mb-4 car-listings" src={Car1} alt="" />
+                      <h4 className="text-uppercase mb-4">Honda Civic Type-R</h4>
+                      <div className="d-flex justify-content-center mb-4">
+                        <div className="px-2">
+                          <i className="fa fa-car text-primary mr-1" />
+                          <span>2019</span>
+                        </div>
+                        <div className="px-2 border-left border-right">
+                          <i className="fa fa-cogs text-primary mr-1" />
+                          <span>MANUAL</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-road text-primary mr-1" />
+                          <span>55K</span>
+                        </div>
+                        <div className="px-2">
+                          <i className="fa fa-map-marker text-primary mr-1" />
+                          <span>Karachi</span>
+                        </div>
+                      </div>
+                      <a className="btn btn-primary px-3" href="">$100.00/Day</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div style={{marginTop: -70}}>
-                <h2 style={{color: '#2b2e4a', fontSize: '150px', fontWeight:'bold',display: 'flex',  justifyContent:'center', alignItems:'center'}}>FIND YOUR CAR</h2>
-            </div>
-
-            <div style={{display:'flex' ,justifyContent:'space-evenly'}}>
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Lahore</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Lahore</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Karachi</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-            </div>
-
-
-
-            <div style={{display:'flex' ,justifyContent:'space-evenly', marginTop:'45px'}}>
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Karachi</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Lahore</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-
-                <div style={{background: '#e2e3e8', display: 'grid', justifyContent:'center', alignItems:'center', height: '50vh', width: '40vh',marginRight:'40px'}}>
-                <img height={200} width={300} src={Nigga} />
-                <h1 style={{color: '#2b2e4a', fontSize: '25px', marginTop:'-20px', display:'flex', fontWeight:'bold', justifyContent:'center', alignItems:'center'}}>Mercedes Benz C180</h1>
-                <div style={{display:'flex' ,justifyContent:'space-between'}}>
-                    <div>
-                        <AiFillCar style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>2015</p>
-                    </div>
-                    <div>
-                        <MdSettingsSuggest style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>AUTO</p>
-                    </div>
-                    <div>
-                        <ImRoad style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'5px'}}>25k</p>
-                    </div>
-                    <div>
-                        <HiLocationMarker style={{height:'30px',width:'30px'}}/>
-                        <p style={{marginLeft:'-5px'}}>Lahore</p>
-                    </div>
-
-                </div>
-                <div className="w-full h-16 flex justify-center">
-                            <button className="flex px-10 py-4 text-white text-2xl font-bold bg-orange-400 ">
-                     $99.00/day
-                 </button>
-             </div>  
-                </div>
-            </div>
-
-
-
-            
-            
-        </div>
-        
-    )
+          );
 }
 export default FindCar;
