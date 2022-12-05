5042d320531f16c00e44bd894a631c49af5075f8
moazahmedd <moaz-ahmed@outlook.com>
2022-12-05 18:57:58 +0500

    feat: Add A Listing, Delete A Listing, Get Listing By Id

--- controllers/listing/index.js
+++ controllers/listing/index.js
@@ -1,23 +1,68 @@
+const { default: mongoose } = require("mongoose");
 const listing = require("../../models/listing");
 
 const getAllListings = async (req, res) => {
-  //   const listedCar = new listing({
-  //     carName: "ALto",
-  //     company: "Suzuki",
-  //     model: 2019,
-  //     mileage: 35000,
-  //     transmission: "manual",
-  //     location: "Lahore",
-  //     rentPerDay: 3000,
-  //     picture:
-  //       "https://cache4.pakwheels.com/system/car_generation_pictures/4989/original/Alto_2019_Exterior-5.jpg?1560326906",
-  //   });
-  //   await listedCar.save();
-
   const listings = await listing.find({});
+  //need to add in filters
   return res.status(200).send({ count: listings.length, listings });
 };
 
+const addListing = async (req, res) => {
+  const { carName, company, model, mileage, transmission, location, rentPerDay, picture } =
+    req.body;
+  const listedCar = new listing({
+    carName,
+    company,
+    model,
+    mileage,
+    transmission,
+    location,
+    rentPerDay,
+    picture,
+    // verified: false,
+  });
+  await listedCar.save();
+
+  //need to cater admin side.. initially verified: false should be there...
+
+  return res.status(200).send({ msg: "Listing Added Successfully" });
+};
+
+const deleteListing = async (req, res) => {
+  const { id } = req.params;
+  const valid = mongoose.isValidObjectId(id);
+
+  if (!id || id <= 0 || !valid) {
+    return res.status(403).send({ msg: "Invalid Id" });
+  }
+
+  const getById = await listing.findById(id);
+  if (!getById) {
+    return res.status(422).send({ msg: "No Such Listing Found!" });
+  }
+
+  await listing.deleteOne({ _id: id });
+
+  res.status(200).send({ msg: "Listing Deleted Successfully!" });
+};
+
+const getById = async (req, res) => {
+  const id = req.params.id;
+  const valid = mongoose.isValidObjectId(id);
+
+  if (!id || id <= 0 || !valid) return res.status(400).send({ msg: "Invalid Id" });
+
+  const item = await listing.findById(id);
+  if (!item) {
+    return res.status(422).send({ msg: "No Such Listing Found!" });
+  }
+
+  res.status(200).send({ item });
+};
+
 module.exports = {
+  getById,
   getAllListings,
+  addListing,
+  deleteListing,
 };
--- routes/listing/index.js
+++ routes/listing/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getAllListings } = require("../../controllers/listing");
-const { Validator } = require("../../middleware/ensure-auth");
+const { getAllListings, addListing, deleteListing, getById } = require("../../controllers/listing");
+const { Validator, ensureAuth } = require("../../middleware/ensure-auth");
+const { validateNew } = require("./validate");
 
 const app = express.Router();
 
 app.get("/", getAllListings);
+app.post("/", ensureAuth, Validator(validateNew, "body"), addListing);
+app.get("/:id", ensureAuth, getById);
+app.delete("/:id", ensureAuth, deleteListing);
 
 module.exports = app;
