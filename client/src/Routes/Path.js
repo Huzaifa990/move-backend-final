afecd212adb26cffec1a8dcd7618818c9b8e1632
CulturalHeritageOfLahore <alphaq990@gmail.com>
2022-12-07 16:23:04 +0500

    Update Listings UI + Integration

--- client/src/Routes/Path.js
+++ client/src/Routes/Path.js
@@ -18,6 +18,7 @@ import SignIn from "../components/SignIn";
 import Listings from "../components/Listings";
 import AddListings from "../components/AddListings";
 import ViewListings from "../components/ViewListings";
+import EditListing from "../components/EditListings";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
             <Route path='listings' element={ <><NavBar/> <Search/> <SubHeaderAbout headingText="Listings"/> <Listings/> <Footer/> </>} />
             <Route path='addListings' element={ <><NavBar/> <Search/> <SubHeaderAbout headingText="List Your Car"/> <AddListings/> <Footer/> </>} />
             <Route path='viewListings' element={ <><NavBar/> <Search/> <SubHeaderAbout headingText="Car Details"/> <ViewListings/> <Footer/> </>} />
+            <Route path='editListings' element={ <><NavBar/> <Search/> <SubHeaderAbout headingText="Edit Details"/> <EditListing/> <Footer/> </>} />
 
         </Routes>
       </BrowserRouter>
