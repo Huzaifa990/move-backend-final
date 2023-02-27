d461998e1f2974378ebff265772c566049e33f17
moazahmedd <moaz-ahmed@outlook.com>
2023-02-27 23:00:55 +0500

    feat: Activate a listing API (admin) + Sign up form changes

--- routes/auth/helper.js
+++ routes/auth/helper.js
@@ -1,5 +1,7 @@
 const passwordPattern = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/);
+const base64ImgPattern = new RegExp(/^data:image\/([a-zA-Z]*);base64,([^"]*)$/);
 
 module.exports = {
   passwordPattern,
+  base64ImgPattern,
 };
