afe54d9a99a5446c7324b5325169f1e0effc9e6f
moazahmedd <moaz-ahmed@outlook.com>
2023-03-15 05:11:58 +0500

    feat: stripe payment

--- routes/booking/index.js
+++ routes/booking/index.js
@@ -12,19 +12,23 @@ const {
   getLessorPendingBookings,
   markAsComplete,
   cancelBooking,
+  checkAndAddBooking,
+  cardPayment,
 } = require("../../controllers/booking");
 const { Validator, ensureAuth } = require("../../middleware/ensure-auth");
-const { validateNew, validateUpdate } = require("./validate");
+const { validateNew, validateUpdate, validateNewCard, validateCardPayment } = require("./validate");
 
 const app = express.Router();
 
 app.get("/", ensureAuth, getAllBookings);
 app.post("/", ensureAuth, Validator(validateNew, "body"), addBooking);
+app.post("/cardBooking", ensureAuth, Validator(validateNewCard, "body"), checkAndAddBooking);
 app.get("/myBookings", ensureAuth, getMyBookings);
 app.get("/getLessorBookings", ensureAuth, getLessorBookings);
 app.get("/getLessorPendingBookings", ensureAuth, getLessorPendingBookings);
 app.get("/:id", ensureAuth, getBookingById);
 app.delete("/:id", ensureAuth, deleteBooking);
+app.put("/stripePayment", ensureAuth, Validator(validateCardPayment, "body"), cardPayment);
 app.put("/approve/:id", ensureAuth, approveBooking);
 app.put("/reject/:id", ensureAuth, rejectBooking);
 app.put("/cancel/:id", ensureAuth, cancelBooking);
